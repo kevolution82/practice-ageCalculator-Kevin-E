51f0285b9d49b1aae3ef5691249ae78856b88dc5
kevdude82 <altvbusiness@gmail.com>
2025-06-03 17:08:29 -0500

    Finished assignment.

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        int yearsLeft = 100 - age;
+
+        System.out.printIn("You's got " + yearsLeft + " years until ya turn 100!  Hooray for math!");
+
+        scanner.close();
+    }
+    }
